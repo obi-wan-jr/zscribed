a5a403210cd3855320eea16e67173bd54d212278
inggo <inggo@example.com>
2025-09-03 15:57:07 +0800

    Fix PM2 script path and add config.json

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'dscribe',
-    script: 'server/index.js',
+    script: './server/index.js',
     cwd: '/home/inggo/dscribe',
     instances: 1,
     exec_mode: 'fork',
